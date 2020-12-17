d2c8f623d5b9690774101095e469637194d1a869
CodeMoogle <codemoogle90@gmail.com>
2020-12-17 19:54:50 +0300

    Added base GameCard component.

---
+++ src/components/Game/GameCard.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="game-card">
+		<img :src="game.background_image" :alt="game.name" />
+		<div class="game-card__content">
+			<h3>{{ game.name }}</h3>
+			<p>Release date: {{ game.released }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "GameCard",
+	props: {
+		game: {
+			type: Object,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+	.game-card {
+		min-height: 30vh;
+		box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+		border-radius: 1rem;
+		overflow: hidden;
+		& img {
+			display: block;
+			width: 100%;
+			height: 40vh;
+			object-fit: cover;
+		}
+		&__content {
+			padding: 0.5rem 1rem;
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,30 @@
 <template>
 	<div class="home">
 		<h1>Games-Ignite</h1>
+
+		<div class="games-list">
+			<h2 class="games-list__title">Upcoming Games</h2>
+			<div class="games-list__content">
+				<GameCard
+					v-for="game in getAllGames.upcomingGames"
+					:key="game.id"
+					:game="game"
+				/>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import GameCard from "@/components/Game/GameCard.vue";
+
 import { mapActions, mapGetters } from "vuex";
 
 export default {
 	name: "Home",
-	components: {},
+	components: {
+		GameCard,
+	},
 	computed: {
 		...mapGetters(["getAllGames"]),
 	},
@@ -21,3 +36,19 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss">
+	.games-list {
+		padding: 0 1rem;
+		min-height: 80vh;
+		&__title {
+			padding: 2rem 0;
+		}
+		&__content {
+			display: grid;
+			grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+			grid-column-gap: 2rem;
+			grid-row-gap: 3rem;
+		}
+	}
+</style>
