fd007035df81d527fa9db0aa87e2d24b11b792cb
CodeMoogle <codemoogle90@gmail.com>
2021-01-14 19:43:56 +0300

    New component GamesPreview added. GamesPreview gets title(string) and games(array) as a props.

---
+++ src/components/Game/GamesPreview.vue
@@ -0,0 +1,71 @@
+<template>
+	<div class="games-preview">
+		<h2 class="games-preview__title">{{ title }}</h2>
+		<splide :options="splideOptions" :slides="games">
+			<splide-slide v-for="game in games" :key="game.id">
+				<GameCard :game="game" />
+			</splide-slide>
+		</splide>
+	</div>
+</template>
+
+<script>
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
+
+import GameCard from "@/components/Game/GameCard.vue";
+
+export default {
+	name: "GamesPreview",
+	components: {
+		Splide,
+		SplideSlide,
+		GameCard,
+	},
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+		games: {
+			type: Array,
+			required: true,
+			default() {
+				return [];
+			},
+		},
+	},
+	data() {
+		return {
+			splideOptions: {
+				rewind: true,
+				perPage: 4,
+				gap: "1rem",
+				// focus: "center",
+				trimSpace: true,
+				breakpoints: {
+					1124: {
+						perPage: 3,
+					},
+					900: {
+						perPage: 2,
+					},
+					600: {
+						perPage: 1,
+					},
+				},
+			},
+		};
+	},
+};
+</script>
+
+<style lang="scss">
+	.games-preview {
+		padding: 0 1rem 2rem 1rem;
+		min-height: 50vh;
+		&__title {
+			padding: 2rem 0;
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,70 +1,25 @@
 <template>
 	<div class="home">
-		<div class="games-list">
-			<h2 class="games-list__title">Upcoming Games</h2>
-			<splide :options="splideOptions" :slides="getAllGames.upcomingGames">
-				<splide-slide v-for="game in getAllGames.upcomingGames" :key="game.id">
-					<GameCard :game="game" />
-				</splide-slide>
-			</splide>
-		</div>
+		<GamesPreview
+			:title="'Upcoming Games'"
+			:games="getAllGames.upcomingGames"
+		/>
 
-		<div class="games-list">
-			<h2 class="games-list__title">New Games</h2>
-			<splide :options="splideOptions" :slides="getAllGames.newGames">
-				<splide-slide v-for="game in getAllGames.newGames" :key="game.id">
-					<GameCard :game="game" />
-				</splide-slide>
-			</splide>
-		</div>
+		<GamesPreview :title="'New Games'" :games="getAllGames.newGames" />
 
-		<div class="games-list">
-			<h2 class="games-list__title">Most Popular</h2>
-			<splide :options="splideOptions" :slides="getAllGames.popularGames">
-				<splide-slide v-for="game in getAllGames.popularGames" :key="game.id">
-					<GameCard :game="game" />
-				</splide-slide>
-			</splide>
-		</div>
+		<GamesPreview :title="'Most Popular'" :games="getAllGames.popularGames" />
 	</div>
 </template>
 
 <script>
-import { Splide, SplideSlide } from "@splidejs/vue-splide";
-import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
-
-import GameCard from "@/components/Game/GameCard.vue";
+import GamesPreview from "@/components/Game/GamesPreview.vue";
 
 import { mapActions, mapGetters } from "vuex";
 
 export default {
 	name: "Home",
 	components: {
-		Splide,
-		SplideSlide,
-		GameCard,
-	},
-	data() {
-		return {
-			splideOptions: {
-				rewind: true,
-				perPage: 4,
-				gap: "1rem",
-				// focus: "center",
-				trimSpace: true,
-				breakpoints: {
-					1124: {
-						perPage: 3,
-					},
-					900: {
-						perPage: 2,
-					},
-					600: {
-						perPage: 1,
-					},
-				},
-			},
-		};
+		GamesPreview,
 	},
 	computed: {
 		...mapGetters(["getAllGames"]),
@@ -79,17 +34,4 @@ export default {
 </script>
 
 <style lang="scss">
-	.games-list {
-		padding: 0 1rem 2rem 1rem;
-		min-height: 50vh;
-		&__title {
-			padding: 2rem 0;
-		}
-		&__content {
-			display: grid;
-			grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-			column-gap: 2rem;
-			row-gap: 2rem;
-		}
-	}
 </style>
